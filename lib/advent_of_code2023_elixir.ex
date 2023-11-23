95b612c64af51cf032345c839761d840aecdacc3
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-11-23 21:30:44 +0100

    Initial commit with project structure

---
+++ lib/advent_of_code2023_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2023Elixir do
+  @moduledoc """
+  Documentation for `AdventOfCode2023Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2023Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code2023_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2023ElixirTest do
+  use ExUnit.Case
+  doctest AdventOfCode2023Elixir
+
+  test "greets the world" do
+    assert AdventOfCode2023Elixir.hello() == :world
+  end
+end
